ee4bc9e76ac7190a5cb417b27a8eb8981e6e51c2
Visweswaran4043 <148361355+Visweswaran4043@users.noreply.github.com>
2023-11-10 15:01:12 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,19 @@
+package Static;
+
+public class Employee 
+{
+private String name;
+private int id;
+static String companyName = "TNSIF";
+public Employee(String name, int id)
+{
+	super();
+	this.name = name;
+	this.id = id;
+}
+@Override
+public String toString() {
+	return "Employee [name=" + name + ", id=" + id + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+			+ ", toString()=" + super.toString() + "]";
+}
+}
